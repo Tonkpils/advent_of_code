93d75f25129ae332ca63a490f34d9c6f1cb409fc
Leo Correa <lcorr005@gmail.com>
2015-12-17 22:20:59 -0700

    [Day 3] Solution Part 2.

--- day3/day3.rb
+++ day3/day3.rb
@@ -2,12 +2,16 @@ input = File.open("day3input.txt")
 
 X = 0
 Y = 1
+ORIGIN = [0,0]
 
 houses_visited = Hash.new(0)
-current_location = [0,0]
-houses_visited[current_location.dup] = 1
+robot_location = ORIGIN.dup
+santas_location = ORIGIN.dup
+houses_visited[ORIGIN.dup] = 2
 
-input.each_char do |c|
+input.each_char.each_with_index do |c, idx|
+  current_location = idx.even? ? santas_location : robot_location
+
   case c
   when '^' then current_location[Y] += 1
   when '>' then current_location[X] += 1
