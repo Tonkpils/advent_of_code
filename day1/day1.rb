6a0b7df428e99b0bada6aba7fa1a5f65de4f3e3c
Leo Correa <lcorr005@gmail.com>
2015-12-17 21:35:07 -0700

    [Day1] Doh! Didn't know there was a part 2

--- day1/day1.rb
+++ day1/day1.rb
@@ -1,10 +1,17 @@
 input = File.read("day1input.txt")
 floor = 0
-input.each_char do |c|
+
+basement_entered_at = nil
+
+input.each_char.each_with_index do |c, idx|
   case c
   when '(' then floor += 1
   when ')' then floor -= 1
   end
+  if basement_entered_at.nil? && floor == -1
+    basement_entered_at = idx + 1
+  end
 end
 
 puts "Floor #{floor}"
+puts "Basement entered at #{basement_entered_at}"
