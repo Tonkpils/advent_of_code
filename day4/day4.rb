e61729eabb947dfe31917358d13d2d2d4773ab69
Leo Correa <lcorr005@gmail.com>
2015-12-17 22:53:05 -0700

    [Day 4] Solution Part 2

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,10 +1,9 @@
 require "digest"
 
 PASSWORD = ENV["AOC_DAY4_PASS"]
-ADVENT_HASH = "00000".freeze
+ADVENT_HASH = "000000".freeze
 IS_ADVENT_HASH = ->(hex) { hex.start_with?(ADVENT_HASH) }
 num = 0
-
 
 loop do
   message = "#{PASSWORD}#{num}"
